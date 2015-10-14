aa033a099d8af9e7abbcaa0bd60004a80e2e0f8e
Michael Church <michael.o.church@gmail.com>
2015-10-14 17:26:19 -0500

    Starting to create the RDD class.

---
+++ Heat/RDD.hs
@@ -0,0 +1,15 @@
+module Heat.RDD where
+
+-- This will evolve into a megaclass... and then I will have to refactor it.
+class RDD r where
+  rMap      :: (a -> b) -> r a -> r b
+  rFilter   :: (a -> Bool) -> r a -> r a
+  rFlatMap  :: (a -> [b]) -> r a -> r b
+  rCount    :: r a -> Integer
+
+-- Silly instance that probably won't live long. 
+instance RDD [] where
+  rMap     = map
+  rFilter  = filter
+  rFlatMap = concatMap
+  rCount   = length
